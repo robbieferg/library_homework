4fa0c9e249b648a0ba2494f509df140e4a746f3b
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-01 18:44:40 +0000

    Book class has values and get methods

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("City Primeval", "Elmore Leonard", "Crime");
+    }
+
+    @Test
+    public void bookHasValues() {
+        assertEquals("City Primeval", book.getTitle());
+        assertEquals("Elmore Leonard", book.getAuthor());
+        assertEquals("Crime", book.getGenre());
+    }
+    
+}
